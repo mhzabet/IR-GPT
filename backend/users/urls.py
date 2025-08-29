72d3f7e6496a5e8ba74384ca28faaa43e00e5b63
mhzabet <zabetzadehmohmmad@gmail.com>
2025-08-29 20:20:05 +0330

    stage

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import UserDetailView
 urlpatterns = [
-    
+    path("user/<int:pk>", UserDetailView.as_view(), name="user-detail")
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,9 +2,28 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.http import Http404
 from .models import CustomUser
+from .serializers import RegisterUserSerializer, BaseUserSerializer
 from .utils import generate_secure_otp
 
 class RegisterUserView(APIView):
-    pass
+    def post(self, request, format=None):
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(is_active=False)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetailView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return CustomUser.objects.get(pk=pk)
+        except CustomUser.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = BaseUserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
