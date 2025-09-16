24dae5c26709409752bebeeada74691663a891a5
mhzabet <zabetzadehmohmmad@gmail.com>
2025-09-16 16:54:11 +0330

    configured admin.py for CustomUser on users app

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,4 +6,12 @@ from django.contrib.admin import register
 
 @register(CustomUser)
 class CustomuUserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['username', 'is_active', 'is_staff', 'is_superuser']
+    list_filter = ['is_active', 'is_staff', 'is_superuser']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),)
